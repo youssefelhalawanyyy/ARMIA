55af1cf8de8d31c1708371306e73af0bb2e425bf
youssef <youssefelhalawany5@gmail.com>
2026-08-31 19:24:36 +0300

    fix: resolve React Flight Server Component enqueueModel error by cleaning RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,13 +73,6 @@ export default function RootLayout({
       lang="en"
       className={`${playfair.variable} ${montserrat.variable} h-full antialiased scroll-smooth`}
     >
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
-        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
-      </head>
       <body className="min-h-full flex flex-col bg-[#F6F3EE] text-[#1F1F1F] font-sans selection:bg-[#DCC9A6] selection:text-[#1F1F1F]">
         <LanguageProvider>
           <PWAProvider>
